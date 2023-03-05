a8d574a5fdafb4250d32beeb0fe7e20c86555d86
Ajay-Dev99 <ajaydevkmd@gmail.com>
2023-03-05 14:38:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import Navbar from "./Component/Navbar/Navbar";
+import {original,drama} from './urls'
+import "./App.css";
+import Banner from "./Component/Banner/Banner";
+import RowPost from "./Component/RowPost/RowPost";
+
+function App() {
+  useEffect(()=>{
+
+  },[])
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+      <RowPost url={original} title='Netflix Originals'/>
+      <RowPost url={drama} title='Drama' isSmall/>
+    </div>
+  );
+}
+
+export default App;
